b14a30a6e7fe76014a24ef32b358840d2dd2d655
honhaocheng <18307650678@163.com>
2026-01-02 18:44:29 +0800

    feat:p163课程代码提交

--- code/newssystem/src/views/sandBox/home/Home.js
+++ code/newssystem/src/views/sandBox/home/Home.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Button } from 'antd';
+import axios from 'axios'
 
 const Home = () => {
+  const ajax = () => {
+    // 取数据
+    // axios.get('http://localhost:8000/posts').then(res => {
+    //   console.log(`res.data`, res.data);
+    // })
+
+    // 增
+    axios.post('http://localhost:8000/posts', {
+      title: '33333',
+      author: 'xiaoming',
+    })
+  }
   return (
     <div>
-      <Button type="primary">Button</Button>
+      <Button type="primary" onClick={ajax}>Button</Button>
     </div>
   );
 }
