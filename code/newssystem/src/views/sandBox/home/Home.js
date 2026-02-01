a4ea6b64d9298370a322cc65a7eb3777a822a3e7
honhaocheng <18307650678@163.com>
2026-02-01 23:16:22 +0800

    feat:p164课程代码提交

--- code/newssystem/src/views/sandBox/home/Home.js
+++ code/newssystem/src/views/sandBox/home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from 'antd';
-import axios from 'axios'
+// import axios from 'axios'
 
 const Home = () => {
   const ajax = () => {
@@ -10,10 +10,33 @@ const Home = () => {
     // })
 
     // 增
-    axios.post('http://localhost:8000/posts', {
-      title: '33333',
-      author: 'xiaoming',
-    })
+    // axios.post('http://localhost:8000/posts', {
+    //   title: '33333',
+    //   author: 'xiaoming',
+    // })
+
+    // 更新 put（替换式更新） 
+    // axios.put('http://localhost:8000/posts/1', {
+    //   title: '1111-修改',
+    // })
+
+    // 更新 patch（补丁式更新）
+    // axios.patch('http://localhost:8000/posts/1', {
+    //   title: '1111-修改-11111',
+    // })
+
+    // 删除 delete
+    // axios.delete('http://localhost:8000/posts/1')
+
+    // _embed
+    // axios.get('http://localhost:8000/posts?_embed=comments').then(res => {
+    //   console.log(`res.data`, res.data)
+    // })
+
+    // _expand
+    // axios.get('http://localhost:8000/comments?_expand=post').then(res => {
+    //   console.log(`res.data`, res.data)
+    // })
   }
   return (
     <div>
